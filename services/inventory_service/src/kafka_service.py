e469c6b4c88f08bd7e5912c95c34ed1771d9cde3
edwingudfriend <edwingudfriend@gmail.com>
2024-05-10 14:58:26 +0300

    updatink kafka and routing in inventory

---
+++ services/inventory_service/src/kafka_service.py
@@ -0,0 +1,9 @@
+import json
+from kafka import KafkaConsumer # type: ignore
+from .config import KAFKA_BOOTSTRAP_SERVERS
+
+consumer = KafkaConsumer(
+    'order_topic',
+    bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+)
